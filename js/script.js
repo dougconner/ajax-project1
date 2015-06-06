189d614c034eef1dbaf14a0578c32da32f9d194b
Doug Conner <dconner@connerbase.com>
2015-06-05 20:51:48 -0700

    feat: initial streetview implementation

--- js/script.js
+++ js/script.js
@@ -14,8 +14,14 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    var street = $("#street").val();
+    var city = $("#city").val();
+    var location = street + ", " + city;
+    var streetReq = '<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + location + '">';
+    console.log(streetReq);
+    $body.append(streetReq);
 
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
