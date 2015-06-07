c63ad9b2508986eb08847eb00a6c6abf6f8ceeeb
Doug Conner <dconner@connerbase.com>
2015-06-07 13:29:55 -0700

    feat: Initial code for NYT AJAX request

--- js/script.js
+++ js/script.js
@@ -23,6 +23,24 @@ function loadData() {
     console.log(streetReq);
     $body.append(streetReq);
 
+    // NY Times AJAX request
+
+    var nytReq = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key=5efb7c1a602e9a6cff740e1f039fc3ce:1:72234776';
+
+    $.getJSON( nytReq, function( data ) {
+      console.log(data);
+      var items = [];
+      $.each( data, function( key, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+
+      $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+      }).appendTo( "body" );
+    });
+
+
     return false;
 }
 
